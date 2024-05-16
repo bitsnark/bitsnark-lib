24c1217d97c089a486a15fc952f5659bcbe3fef3
Derek Matter <dharkmattr@gmail.com>
2024-05-16 17:58:54 +0300

    wip

--- src/generator/common/state.ts
+++ src/generator/common/state.ts
@@ -5,11 +5,22 @@ export class State {
     registers: Register[] = [];
     maxRegCount = 0;
 
-    newRegister(): Register {
+    newRegister(n?: bigint): Register {
         let r: Register = new Register();
         r.index = this.registers.length;
         this.registers[r.index] = r;
         this.maxRegCount = Math.max(this.maxRegCount, this.registers.length);
+        r.value = n ?? 0n;
+        return r;
+    }
+
+    newHardcoded(n: bigint) {
+        let r: Register = new Register();
+        r.index = this.registers.length;
+        this.registers[r.index] = r;
+        this.maxRegCount = Math.max(this.maxRegCount, this.registers.length);
+        r.value = n ?? 0n;
+        r.hardcoded = true;
         return r;
     }
 }
