a926b115536cef7ec544d330292dd6327cd3b288
Derek Matter <dharkmattr@gmail.com>
2024-06-12 17:12:28 +0300

    wip

---
+++ src/generator/step2/vm/types.ts
@@ -0,0 +1,23 @@
+export enum InstrCode {
+    ADDMOD = 'ADDMOD',
+    ANDBIT = 'ANDBIT',
+    ANDNOTBIT = 'ANDNOTBIT',
+    MOV = 'MOV',
+    EQUAL = 'EQUAL',
+    MULMOD = 'MULMOD',
+    OR = 'OR',
+    AND = 'AND',
+    NOT = 'NOT',
+    SUBMOD = 'SUBMOD',
+    DIVMOD = 'DIVMOD',
+    ASSERTONE = 'ASSERTONE',
+    ASSERTZERO = 'ASSERTZERO'
+}
+
+export interface Instruction {
+    name: InstrCode;
+    target: number;
+    param1: number;
+    param2?: number;
+    data?: bigint;
+}
