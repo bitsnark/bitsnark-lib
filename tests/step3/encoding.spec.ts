2c079b7ce07e156d3953a4157147e1b75687d87b
gula <gula@deweb.io>
2024-07-04 14:58:17 +0300

    lamport + Bitcoin demo test

--- tests/step3/encoding.spec.ts
+++ tests/step3/encoding.spec.ts
@@ -1,12 +1,14 @@
+import { deleteDir } from "../../src/encoder-decoder/files-utils";
+import { Lamport } from "../../src/encoder-decoder/lamport";
 import { Bitcoin } from "../../src/generator/step3/bitcoin";
 import { StackItem } from "../../src/generator/step3/stack";
-import { strToBigint, writeBigintToBuffer, hash, bufferToBigints256, Key, bitsToBigint, nibblesToBigint } from "./utils";
+import { strToBigint, writeBigintToBuffer, hash, bufferToBigints256, Key, bitsToBigint, nibblesToBigint, bufferToBigints256BE } from "./utils";
 
 const testData32Bits = strToBigint('TEST');
 const testData256Bits = strToBigint('TESTING1TESTING2TESTING3TESTING4');
 
 const lamportKeys: Key[][] = [];
-for (let i = 0; i < 256; i++) lamportKeys.push([ 
+for (let i = 0; i < 256; i++) lamportKeys.push([
     { prvt: hash(BigInt(i), 1), pblc: hash(BigInt(i), 2) },
     { prvt: hash(1000000n + BigInt(i), 1), pblc: hash(1000000n + BigInt(i), 2) }
 ]);
@@ -24,7 +26,14 @@ function encodeLamportBits(input: bigint, bits: number): Buffer {
         encodeLamportBit(output, i, Number(input & 1n));
         input = input >> 1n;
     }
+
     return output;
+}
+
+function encodeLamportBuffer(input: bigint, bits: number, lamport: Lamport) {
+    const buffer = Buffer.alloc(bits);
+    writeBigintToBuffer(buffer, 0, input, bits);
+    return lamport.encodeBuffer(buffer, bits);
 }
 
 const winternitzKeys: Key[] = [];
@@ -63,12 +72,20 @@ function encodeWinternitz(input: bigint, dataBits: number, checksumBits: number)
     return output;
 }
 
+function generatLamport(folder: string, bits: number) {
+    deleteDir(folder);
+    const lamport = new Lamport(folder);
+    lamport.generateKeys(bits);
+    return lamport;
+}
 describe("encoding schemes", function () {
 
     let bitcoin: Bitcoin;
+    let lamport: Lamport;
     let encoded;
     let witness: StackItem[];
     let decodedItems: StackItem[];
+
 
     describe('lamport 32 bits', () => {
 
@@ -76,14 +93,28 @@ describe("encoding schemes", function () {
 
         beforeEach(() => {
             bitcoin = new Bitcoin();
-            encoded = encodeLamportBits(testData32Bits, 32);
-            witness = bufferToBigints256(encoded).map(n => bitcoin.addWitness(n));
+
+            lamport = generatLamport('lamport32', 32);
+            const buffer = Buffer.alloc(4);
+            writeBigintToBuffer(buffer, 0, testData32Bits, 4);
+            const { encodedData, pubk } = lamport.encodeBufferAddPublic(buffer, 0);
+            encoded = encodedData;
+
+            witness = bufferToBigints256BE(encoded).map(n => bitcoin.addWitness(n));
+
+
             keyItems = [];
             for (let i = 0; i < 32; i++) {
-                keyItems.push(lamportKeys[i].map(k => k.pblc));
+                keyItems.push(
+                    [
+                        bufferToBigints256BE(pubk.subarray(i * 64, i * 64 + 32))[0],
+                        bufferToBigints256BE(pubk.subarray(i * 64 + 32, (i + 1) * 64))[0]
+                    ]);
             }
+
             decodedItems = [];
             for (let i = 0; i < 32; i++) decodedItems.push(bitcoin.newStackItem(0n));
+
         });
 
         it("positive", async () => {
@@ -113,11 +144,21 @@ describe("encoding schemes", function () {
 
         beforeEach(() => {
             bitcoin = new Bitcoin();
-            encoded = encodeLamportBits(testData256Bits, 256);
-            witness = bufferToBigints256(encoded).map(n => bitcoin.addWitness(n));
+
+            lamport = generatLamport('lamport256', 256);
+            const buffer = Buffer.alloc(32);
+            writeBigintToBuffer(buffer, 0, testData256Bits, 32);
+            const { encodedData, pubk } = lamport.encodeBufferAddPublic(buffer, 0);
+            encoded = encodedData;
+
+            witness = bufferToBigints256BE(encoded).map(n => bitcoin.addWitness(n));
             keyItems = [];
             for (let i = 0; i < 256; i++) {
-                keyItems.push(lamportKeys[i].map(k => k.pblc));
+                keyItems.push(
+                    [
+                        bufferToBigints256BE(pubk.subarray(i * 64, i * 64 + 32))[0],
+                        bufferToBigints256BE(pubk.subarray(i * 64 + 32, (i + 1) * 64))[0]
+                    ]);
             }
             decodedItems = [];
             for (let i = 0; i < 256; i++) decodedItems.push(bitcoin.newStackItem(0n));
