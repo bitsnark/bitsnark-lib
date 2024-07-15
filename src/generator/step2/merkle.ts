266ad83252fbca72bb091acf6c410d5e63c09878
Dr Phill Edwards <phill.edwards@dr-phill-edwards.eu>
2024-07-15 10:06:18 +0200

    Significantly reduced sha256 and merkle register usage

--- src/generator/step2/merkle.ts
+++ src/generator/step2/merkle.ts
@@ -1,93 +1,21 @@
 import { step2_vm } from "./vm/vm";
 import { Register } from "../common/register";
-import { sha256pair } from "./sha-256"
+import { SHA256 } from "./sha-256"
+import { _256 } from "./vm/types"
 
-export class Merkle {
-    private tree: Register[][][]
-
-    public constructor(transactions: Register[][]) {
-        this.tree = [transactions]
-        if ((transactions.length & 1) != 0) {
-            this.tree[0].push(step2_vm.newTemp256(true))
-        }
-        let llen = this.tree[0].length
-        for (let i = 0; llen > 1; i++) {
-            llen = this.MakeLayer(this.tree[i])
-        }
-    }
-
-    public GetRoot() : Register[] {
-        let root = step2_vm.newTemp256(true)
-        let value = this.tree[this.tree.length - 1][0]
-        for (let i = 0; i < 8; i++) {
-            step2_vm.mov(root[i], value[i])
-        }
-        return root
-    }
-
-    public GetProof(index : number) : Register[][] {
-        let proof: Register[][] = []
-        for (let i = 0; i < this.tree.length - 1; i++) {
-            proof.push(step2_vm.newTemp256(true))
-            let hash = this.tree[i][index^1]
-            for (let j = 0; j < 8; j++) {
-                step2_vm.mov(proof[i][j], hash[j])
-            }
-            index = index >> 1
-        }
-        return proof
-    }
-
-    public Free() {
-        for (let i = 0; i < this.tree.length; i++) {
-            let layer = this.tree[i]
-            for (let j = 0; j < layer.length; j++) {
-                let node = layer[j]
-                for (let k = 0; k < node.length; k++) {
-                    step2_vm.freeRegister(node[k])
-                }
-            }
-        }
-    }
-
-    private MakeLayer(inputs: Register[][]) : number {
-        let ilen = inputs.length
-        let llen = ilen >> 1
-        let layer: Register[][] = []
-        for (let i = 0, j = 0; j < llen; j++) {
-            let hash = step2_vm.newTemp256(true)
-            sha256pair(hash, inputs[i], inputs[i + 1])
-            layer.push(hash)
-            i += 2
-        }
-        if (llen > 1 && (llen & 1) != 0) {
-            layer.push(step2_vm.newTemp256(true))
-        }
-        this.tree.push(layer)
-        return layer.length
-    }
-}
-
-export function verifyMerkleProof(index: number, proof: Register[][]) {
-    let hash: Register[] = [];
-    let tempHash: Register[] = [];
-    for (let i = 0; i < 8; i++) {
-        hash.push(step2_vm.newRegister());
-        step2_vm.mov(hash[i], proof[0][i]);
-        tempHash.push(step2_vm.newRegister());
-    }
+export function verifyMerkleProof(proof: _256[], index: number) {
+    const sha256 = new SHA256();
     for (let i = 1; i < proof.length - 1; i++) {
         if ((index & 1) == 0) {
-            sha256pair(tempHash, hash, proof[i]);
+            sha256.sha256pair(proof[0], proof[0], proof[i]);
         } else {
-            sha256pair(tempHash, proof[i], hash);
+            sha256.sha256pair(proof[0], proof[i], proof[0]);
         }
-        for (let j = 0; j < 8; j++) step2_vm.mov(hash[j], tempHash[j]);
         index = index >> 1;
     }
+    sha256.free();
     for (let i = 0; i < 8; i++) {
-        step2_vm.assertEq(hash[i], proof[proof.length - 1][i]);
+        step2_vm.assertEq(proof[0][i], proof[proof.length-1][i]);
+        if (proof[0][i] != proof[proof.length-1][i]) break;
     }
-    step2_vm.freeRegister(hash);
-    step2_vm.freeRegister(tempHash);
 }
--- src/generator/step2/sha-256.ts
+++ src/generator/step2/sha-256.ts
@@ -2,9 +2,11 @@ import { Register } from "../common/register";
 import { step2_vm as vm } from "./vm/vm";
 import { _256 } from "./vm/types"
 
-const h_hex = [0x6a09e667n, 0xbb67ae85n, 0x3c6ef372n, 0xa54ff53an, 0x510e527fn, 0x9b05688cn, 0x1f83d9abn, 0x5be0cd19n]
+const mask32 = 0xffffffffn;
 
-const k_hex = [0x428a2f98n, 0x71374491n, 0xb5c0fbcfn, 0xe9b5dba5n, 0x3956c25bn, 0x59f111f1n, 0x923f82a4n,
+const hHex = [0x6a09e667n, 0xbb67ae85n, 0x3c6ef372n, 0xa54ff53an, 0x510e527fn, 0x9b05688cn, 0x1f83d9abn, 0x5be0cd19n];
+
+const kHex = [0x428a2f98n, 0x71374491n, 0xb5c0fbcfn, 0xe9b5dba5n, 0x3956c25bn, 0x59f111f1n, 0x923f82a4n,
     0xab1c5ed5n, 0xd807aa98n, 0x12835b01n, 0x243185ben, 0x550c7dc3n, 0x72be5d74n, 0x80deb1fen,
     0x9bdc06a7n, 0xc19bf174n, 0xe49b69c1n, 0xefbe4786n, 0x0fc19dc6n, 0x240ca1ccn, 0x2de92c6fn,
     0x4a7484aan, 0x5cb0a9dcn, 0x76f988dan, 0x983e5152n, 0xa831c66dn, 0xb00327c8n, 0xbf597fc7n,
@@ -13,185 +15,183 @@ const k_hex = [0x428a2f98n, 0x71374491n, 0xb5c0fbcfn, 0xe9b5dba5n, 0x3956c25bn, 
     0xc24b8b70n, 0xc76c51a3n, 0xd192e819n, 0xd6990624n, 0xf40e3585n, 0x106aa070n, 0x19a4c116n,
     0x1e376c08n, 0x2748774cn, 0x34b0bcb5n, 0x391c0cb3n, 0x4ed8aa4an, 0x5b9cca4fn, 0x682e6ff3n,
     0x748f82een, 0x78a5636fn, 0x84c87814n, 0x8cc70208n, 0x90befffan, 0xa4506cebn, 0xbef9a3f7n,
-    0xc67178f2n]
+    0xc67178f2n];
 
-function ch(target: Register, x: Register, y: Register, z: Register) {
-    let t1 = vm.newRegister()
-    let t2 = vm.newRegister()
-    vm.and(t1, x, y)
-    vm.not(t2, x)
-    vm.and(t2, t2, z)
-    vm.xor(target, t1, t2)
-    vm.freeRegister(t1)
-    vm.freeRegister(t2)
-}
+const mask32Reg = vm.hardcode(mask32);
+const hReg = vm.initHardcoded(hHex);
+const kReg = vm.initHardcoded(kHex);
+const Sigma0Reg = vm.initHardcoded([2n, 13n, 22n]);
+const Sigma1Reg = vm.initHardcoded([6n, 11n, 25n]);
+const sigma0Reg = vm.initHardcoded([7n, 18n, 3n]);
+const sigma1Reg = vm.initHardcoded([17n, 19n, 10n]);
+const shabitReg = vm.hardcode(0x80000000n);
+const shalen1 = vm.hardcode(256n);
+const shalen2 = vm.hardcode(512n);
 
-function maj(target: Register, x: Register, y: Register, z: Register) {
-    let t1 = vm.newRegister()
-    let t2 = vm.newRegister()
-    let t3 = vm.newRegister()
-    vm.and(t1, x, y)
-    vm.and(t2, x, z)
-    vm.and(t3, y, z)
-    vm.xor(target, t1, t2)
-    vm.xor(target, target, t3)
-    vm.freeRegister(t1)
-    vm.freeRegister(t2)
-    vm.freeRegister(t3)
-}
+export class SHA256 {
+    W: Register[] = [];
+    t1: Register;
+    T0: Register;
+    T1: Register;
+    T2: Register;
+    hash: Register[] = [];
+    h: Register[] = [];
 
-function bigsigma(target: Register, x: Register, a: bigint, b: bigint, c: bigint) {
-    let t0 = vm.newRegister()
-    let t1 = vm.newRegister()
-    let t2 = vm.newRegister()
-    let t3 = vm.newRegister()
-    vm.setRegister(t0, a)
-    vm.rotr(t1, x, t0)
-    vm.setRegister(t0, b)
-    vm.rotr(t2, x, t0)
-    vm.setRegister(t0, c)
-    vm.rotr(t3, x, t0)
-    vm.xor(target, t1, t2)
-    vm.xor(target, target, t3)
-    vm.freeRegister(t0)
-    vm.freeRegister(t1)
-    vm.freeRegister(t2)
-    vm.freeRegister(t3)
-}
-
-function sigma(target: Register, x: Register, a: bigint, b: bigint, c: bigint) {
-    let t0 = vm.newRegister()
-    let t1 = vm.newRegister()
-    let t2 = vm.newRegister()
-    let t3 = vm.newRegister()
-    vm.setRegister(t0, a)
-    vm.rotr(t1, x, t0)
-    vm.setRegister(t0, b)
-    vm.rotr(t2, x, t0)
-    vm.setRegister(t0, c)
-    vm.shr(t3, x, t0)
-    vm.xor(target, t1, t2)
-    vm.xor(target, target, t3)
-    vm.freeRegister(t0)
-    vm.freeRegister(t1)
-    vm.freeRegister(t2)
-    vm.freeRegister(t3)
-}
-
-export function hash(target: Register[], a: Register[]) {
-    const mask = vm.newRegister()
-    vm.setRegister(mask, 0xffffffffn)
-    const T1 = vm.newRegister()
-    const T2 = vm.newRegister()
-    const tmp1 = vm.newRegister()
-    const tmp2 = vm.newRegister()
-    const W: Register[] = []
-    for (let i = 0; i < 16; i++) {
-        W.push(vm.newRegister())
-        vm.mov(W[i], a[i])
-    }
-    for (let i = 16; i < 64; i++) {
-        W.push(vm.newRegister());
-        sigma(T1, W[i-2], 17n, 19n, 10n)
-        sigma(T2, W[i-15], 7n, 18n, 3n)
-        vm.add(W[i], T1, W[i-7])
-        vm.add(W[i], W[i], T2)
-        vm.add(W[i], W[i], W[i-16])
-        vm.and(W[i], W[i], mask)
+    constructor() {
+        this.T0 = vm.newRegister(true);
+        this.T1 = vm.newRegister(true);
+        this.T2 = vm.newRegister(true);
+        this.t1 = vm.newRegister(true);
+        for (let i = 0; i < 8; i++) {
+            this.hash[i] = vm.newRegister(true);
+            this.h[i] = vm.newRegister(true);
+        }
     }
 
-    const h: Register[] = []
-    for (let i = 0; i < 8; i++) {
-        h.push(vm.newRegister())
-        vm.mov(h[i], target[i])
+    free() {
+        vm.freeRegister(this.T0);
+        vm.freeRegister(this.T1);
+        vm.freeRegister(this.T2);
+        vm.freeRegister(this.t1);
+        for (let i = 0; i < 8; i++) {
+            vm.freeRegister(this.hash[i]);
+            vm.freeRegister(this.h[i]);
+        }
+    }
+    
+    ch(target: Register, x: Register, y: Register, z: Register) {
+        vm.and(target, x, y);
+        vm.not(this.t1, x);
+        vm.and(this.t1, this.t1, z);
+        vm.xor(target, target, this.t1);
     }
 
-    for (let i = 0; i < 64; i++) {
-        bigsigma(tmp1, h[4], 6n, 11n, 25n)
-        ch(tmp2, h[4], h[5], h[6])
-        vm.add(T1, h[7], tmp1)
-        vm.add(T1, T1, tmp2)
-        vm.setRegister(T2, k_hex[i])
-        vm.add(T1, T1, T2)
-        vm.add(T1, T1, W[i])
-        vm.and(T1, T1, mask)
-
-        bigsigma(tmp1, h[0], 2n, 13n, 22n)
-        maj(tmp2, h[0], h[1], h[2])
-        vm.add(T2, tmp1, tmp2)
-        vm.and(T2, T2, mask)
-
-        vm.mov(h[7], h[6])
-        vm.mov(h[6], h[5])
-        vm.mov(h[5], h[4])
-        vm.mov(h[4], h[3])
-        vm.add(h[4], h[4], T1)
-        vm.and(h[4], h[4], mask)
-        vm.mov(h[3], h[2])
-        vm.mov(h[2], h[1])
-        vm.mov(h[1], h[0])
-        vm.mov(h[0], T1)
-        vm.add(h[0], h[0], T2)
-        vm.and(h[0], h[0], mask)
+    maj(target: Register, x: Register, y: Register, z: Register) {
+        vm.and(target, x, y);
+        vm.and(this.t1, x, z);
+        vm.xor(target, target, this.t1);
+        vm.and(this.t1, y, z);
+        vm.xor(target, target, this.t1);
+    }
+    
+    bigsigma0(target: Register, x: Register) {
+        vm.rotr(target, x, Sigma0Reg[0]);
+        vm.rotr(this.t1, x, Sigma0Reg[1]);
+        vm.xor(target, target, this.t1);
+        vm.rotr(this.t1, x, Sigma0Reg[2]);
+        vm.xor(target, target, this.t1);
     }
 
-    for (let i = 0; i < 8; i++) {
-        vm.add(target[i], target[i], h[i])
-        vm.and(target[i], target[i], mask)
+    bigsigma1(target: Register, x: Register) {
+        vm.rotr(target, x, Sigma1Reg[0]);
+        vm.rotr(this.t1, x, Sigma1Reg[1]);
+        vm.xor(target, target, this.t1);
+        vm.rotr(this.t1, x, Sigma1Reg[2]);
+        vm.xor(target, target, this.t1);
     }
 
-    vm.freeRegister(mask)
-    vm.freeRegister(T1)
-    vm.freeRegister(T2)
-    vm.freeRegister(tmp1)
-    vm.freeRegister(tmp2)
-    for (let i = 0; i < 8; i++) {
-        vm.freeRegister(h[i])
+    sigma0(target: Register, x: Register) {
+        vm.rotr(target, x, sigma0Reg[0]);
+        vm.rotr(this.t1, x, sigma0Reg[1]);
+        vm.xor(target, target, this.t1);
+        vm.shr(this.t1, x, sigma0Reg[2]);
+        vm.xor(target, target, this.t1);
     }
-    for (let i = 0; i < 64; i++) {
-        vm.freeRegister(W[i]);
-    }
-}
 
-export function sha256(target: _256, a: _256) {
-    for (let i = 0 ; i < 8 ; i++) {
-        vm.setRegister(target[i], h_hex[i])
+    sigma1(target: Register, x: Register) {
+        vm.rotr(target, x, sigma1Reg[0]);
+        vm.rotr(this.t1, x, sigma1Reg[1]);
+        vm.xor(target, target, this.t1);
+        vm.shr(this.t1, x, sigma1Reg[2]);
+        vm.xor(target, target, this.t1);
     }
-    let w: Register[] = []
-    for (let i = 0; i < 16; i++) {
-        w.push(vm.newRegister(true))
-    }
-    for (let i = 0; i < 8; i++) {
-        vm.mov(w[i], a[i])
-    }
-    vm.setRegister(w[8], 0x80000000n)
-    vm.setRegister(w[15], 256n)
-    hash(target, w)
-    for (let i = 0; i < 16; i++) {
-        vm.freeRegister(w[i])
-    }
-}
 
-export function sha256pair(target: _256, a: _256, b: _256) {
-    for (let i = 0 ; i < 8 ; i++) {
-        vm.setRegister(target[i], h_hex[i])
+    calculateW(index: number) {
+        vm.add(this.W[index], this.W[index], this.W[(index+9) & 0xf]);
+        this.sigma1(this.T1, this.W[(index+14) & 0xf]);
+        vm.add(this.W[index], this.W[index], this.T1);
+        this.sigma0(this.T1, this.W[(index+1) & 0xf]);
+        vm.add(this.W[index], this.W[index], this.T1);
+        vm.and(this.W[index], this.W[index], mask32Reg);
     }
-    let w: Register[] = []
-    for (let i = 0; i < 16; i++) {
-        w.push(vm.newRegister(true))
+
+    calculateHash() {
+        for (let i = 0; i < 8; i++) {
+            vm.mov(this.h[i], this.hash[i]);
+        }
+        for (let i = 0; i < 4; i++) {
+            const block = i * 16;
+            for (let j = 0; j < 16; j++) {
+                if ( i > 0) {
+                    this.calculateW(j);
+                }
+                this.bigsigma1(this.T1, this.h[4]);
+                vm.add(this.T1, this.T1, this.h[7]);
+                this.ch(this.T0, this.h[4], this.h[5], this.h[6]);
+                vm.add(this.T1, this.T1, this.T0);
+                vm.add(this.T1, this.T1, kReg[block+j]);
+                vm.add(this.T1, this.T1, this.W[j]);
+                vm.and(this.T1, this.T1, mask32Reg);
+
+                this.bigsigma0(this.T2, this.h[0]);
+                this.maj(this.T0, this.h[0], this.h[1], this.h[2]);
+                vm.add(this.T2, this.T2, this.T0);
+                vm.and(this.T2, this.T2, mask32Reg);
+
+                vm.mov(this.h[7], this.h[6]);
+                vm.mov(this.h[6], this.h[5]);
+                vm.mov(this.h[5], this.h[4]);
+                vm.mov(this.h[4], this.h[3]);
+                vm.add(this.h[4], this.h[4], this.T1);
+                vm.and(this.h[4], this.h[4], mask32Reg);
+                vm.mov(this.h[3], this.h[2]);
+                vm.mov(this.h[2], this.h[1]);
+                vm.mov(this.h[1], this.h[0]);
+                vm.mov(this.h[0], this.T1);
+                vm.add(this.h[0], this.h[0], this.T2);
+                vm.and(this.h[0], this.h[0], mask32Reg);
+            }
+        }
+        for (let i = 0; i < 8; i++) {
+            vm.add(this.hash[i], this.hash[i], this.h[i]);
+            vm.and(this.hash[i], this.hash[i], mask32Reg);
+        }
     }
-    for (let i = 0; i < 8; i++) {
-        vm.mov(w[i], a[i])
-        vm.mov(w[i+8], b[i])
+
+    sha256(target: _256, a: _256) {
+        for (let i = 0 ; i < 8 ; i++) {
+            vm.mov(this.hash[i], hReg[i]);
+        }
+        for (let i = 0; i < 8; i++) {
+            this.W[i] = a[i];
+            this.W[i+8] = vm.newRegister(true);
+        }
+        vm.mov(this.W[8], shabitReg);
+        vm.mov(this.W[15], shalen1);
+        this.calculateHash()
+        for (let i = 0 ; i < 8 ; i++) {
+            vm.mov(target[i], this.hash[i]);
+            vm.freeRegister(this.W[i+8]);
+        }
     }
-    hash(target, w)
-    for (let i = 0; i < 16; i++) {
-        vm.setRegister(w[i], 0n)
-    }
-    vm.setRegister(w[0], 0x80000000n)
-    vm.setRegister(w[15], 512n)
-    hash(target, w)
-    for (let i = 0; i < 16; i++) {
-        vm.freeRegister(w[i])
+
+    sha256pair(target: _256, a: _256, b: _256) {
+        for (let i = 0 ; i < 8 ; i++) {
+            vm.mov(this.hash[i], hReg[i]);
+        }
+        for (let i = 0; i < 8; i++) {
+            this.W[i] = a[i];
+            this.W[i+8] = b[i];
+        }
+        this.calculateHash()
+        for (let i = 1; i < 15; i++) {
+            vm.mov(this.W[i], vm.zero);
+        }
+        vm.mov(this.W[0], shabitReg);
+        vm.mov(this.W[15], shalen2);
+        this.calculateHash();
+        for (let i = 0 ; i < 8 ; i++) {
+            vm.mov(target[i], this.hash[i]);
+        }
     }
 }
