ed3c4f79cb252f5654c745f0111868367c527d12
root <root@chakra>
2024-10-01 11:42:27 +0300

    Indexing, ordered traversal and alignments

--- analysis/generate-dot.ts
+++ analysis/generate-dot.ts
@@ -7,56 +7,32 @@ const TRANSACTION_SHAPE = 'note';
 const PROVER_COLOR = 'green';
 const VERIFIER_COLOR = 'blue';
 const TIMEOUT_STYLE = 'dashed';
-const DEFAULT_WEIGHT = 1;
-const TIMEOUT_WEIGHT = 1;
-const LOCKED_WEIGHT = 1;
-const SELECT_AND_STATE_WEIGHT = 1;
 const OUTPUT_NODE_PROPERTIES = {shape: 'point'};
-const OUTPUT_EDGE_PROPERTIES = {weight: 1, arrowhead: 'none'};
+const OUTPUT_EDGE_PROPERTIES = {arrowhead: 'none'};
+const LOCKED_FUNDS_OUTPUT_WEIGHT = 20;
+const VERTICAL_ALIGNMENT_WEIGHTS: { [key: string]: number } = {
+    mainSteps: 20,
+    stateUncontested: 40,
+    selectUncontested: 0
+};
 
 function dot(transactions: Transaction[]): string {
 
+    const index: { [key: string]: [Transaction, Input][][] } = transactions.reduce(
+        (index: { [key: string]: [Transaction, Input][][] }, transaction) => {
+            transaction.inputs.forEach(input => {
+                index[input.transactionName] ??= [];
+                index[input.transactionName][input.outputIndex] ??= [];
+                index[input.transactionName][input.outputIndex].push([transaction, input]);
+            });
+            return index;
+        }, {});
 
     function properties(properties: { [key: string]: string | number | undefined }): string {
         return `[${Object.entries(properties)
             .filter(([_, value]) => value !== undefined)
             .map(([key, value]) => `${key}=${value}`)
             .join('; ')}]`;
-    }
-
-    function inputProperties(transaction: Transaction, input: Input): string {
-        const output = findOutputByInput(transactions, input);
-        const condition = output.spendingConditions[input.spendingConditionIndex];
-        const style = condition.timeoutBlocks ? TIMEOUT_STYLE : undefined;
-        let weight = DEFAULT_WEIGHT;
-        let label;
-        if (input.transactionName === TransactionNames.LOCKED_FUNDS) weight = LOCKED_WEIGHT;
-        else if (condition.timeoutBlocks) {
-            weight = TIMEOUT_WEIGHT;
-            label = `"timelocked for ${condition.timeoutBlocks} blocks"`;
-
-        } else {
-            const stateOrSelectMatches = [transaction.transactionName, input.transactionName].map(
-                name => name?.match(/^(select|state)_(\d{2})$/)).filter(Boolean);
-            if (
-                (stateOrSelectMatches[0]?.[1] === 'state' && stateOrSelectMatches[1]?.[1] === 'select') ||
-                (stateOrSelectMatches[0]?.[1] === 'select' && stateOrSelectMatches[1]?.[1] === 'state')
-            ) weight = SELECT_AND_STATE_WEIGHT;
-        }
-        return properties({ style, weight, label });
-    }
-
-    function inputLine(transaction: Transaction, input: Input): string {
-        return `${input.transactionName}_output_${input.outputIndex} -> ${transaction.transactionName} ` +
-            `${inputProperties(transaction, input)}`;
-    }
-
-    function outputLines(transaction: Transaction, outputIndex: number): string[] {
-        return [
-            `${transaction.transactionName}_output_${outputIndex} ${properties(OUTPUT_NODE_PROPERTIES)}`,
-            `${transaction.transactionName} -> ${transaction.transactionName}_output_${outputIndex} ` +
-                `${properties(OUTPUT_EDGE_PROPERTIES)}`
-        ];
     }
 
     function transactionProperties(transaction: Transaction): string {
@@ -67,16 +43,98 @@ function dot(transactions: Transaction[]): string {
         });
     }
 
-    function transactionLines(transaction: Transaction): string[] {
+    function transactionLine(transaction: Transaction): string {
+        return `${transaction.transactionName} ${transactionProperties(transaction)}`;
+    }
+
+    function edgeProperties(
+        transaction: Transaction, outputIndex: number, childTransaction: Transaction, childInput: Input
+    ): string {
+        const condition = transaction.outputs[outputIndex].spendingConditions[childInput.spendingConditionIndex];
+        return properties({
+            color: condition.nextRole === AgentRoles.PROVER ? PROVER_COLOR : VERIFIER_COLOR,
+            style: condition.timeoutBlocks ? TIMEOUT_STYLE : undefined,
+            label: condition.timeoutBlocks ? `"${condition.timeoutBlocks} blocks"` : undefined,
+        });
+    }
+
+    function isMultiparousOutput(transaction: Transaction, outputIndex: number): boolean {
+        return (
+            index[transaction.transactionName] &&
+            index[transaction.transactionName][outputIndex] &&
+            index[transaction.transactionName][outputIndex].length > 1) || false;
+    }
+
+    function outputLine(transaction: Transaction, outputIndex: number): string[] {
+        const isMulti = isMultiparousOutput(transaction, outputIndex);
+        const connection = transaction.transactionName + (isMulti ? `_output_${outputIndex}` : '');
         return [
-            `${transaction.transactionName} ${transactionProperties(transaction)}`,
-            ...transaction.inputs.map(input => inputLine(transaction, input)),
-            ...transaction.outputs.flatMap((_, outputIndex) => outputLines(transaction, outputIndex))
+            ...(isMulti ? [
+                `${connection} ${properties(OUTPUT_NODE_PROPERTIES)}`,
+                `${transaction.transactionName} -> ${connection} ${properties({...OUTPUT_EDGE_PROPERTIES, weight: (
+                    transaction.transactionName === TransactionNames.LOCKED_FUNDS ?
+                        LOCKED_FUNDS_OUTPUT_WEIGHT : undefined
+                )})}`] : []),
+            ...((index[transaction.transactionName] && index[transaction.transactionName][outputIndex].map(
+                ([childTransaction, input]) => `${connection} -> ${childTransaction.transactionName} `
+                    + `${edgeProperties(transaction, outputIndex, childTransaction, input)}`)) ?? [])
+        ];}
+
+    function outputLines(transaction: Transaction): string[] {
+        return transaction.outputs.flatMap((output, outputIndex) => outputLine(transaction, outputIndex));
+    }
+
+    function horizontalAlignmentLine(group: Transaction[], rank: string): string {
+        return `{rank=${rank}; ${group.map(transaction => transaction.transactionName).join('; ')}}`;
+    }
+
+    function verticalAlignmentLine(transactions: Transaction[], weight: number): string {
+        return transactions.map(transaction => transaction.transactionName).join(' -> ') +
+            ` [style=invis; weight=${weight}]`;
+    }
+
+    function verticalAlignmentLines(roots: Transaction[]): string[] {
+        const collected: { [key: string]: Transaction[] } = {
+            mainSteps: [], stateUncontested: [], selectUncontested: []};
+        const visited: Set<string> = new Set();
+        function collect(transaction: Transaction) {
+            if (visited.has(transaction.transactionName)) return;
+            visited.add(transaction.transactionName);
+
+            if (transaction.transactionName.startsWith(TransactionNames.STATE_UNCONTESTED))
+                collected.stateUncontested.push(transaction);
+            else if (transaction.transactionName.startsWith(TransactionNames.SELECT_UNCONTESTED))
+                collected.selectUncontested.push(transaction);
+            else if (
+                transaction.transactionName === TransactionNames.PROOF ||
+                transaction.transactionName.startsWith(TransactionNames.STATE) ||
+                transaction.transactionName.startsWith(TransactionNames.SELECT) ||
+                transaction.transactionName === TransactionNames.ARGUMENT ||
+                transaction.transactionName === TransactionNames.PROOF_REFUTED
+            ) collected.mainSteps.push(transaction);
+
+            for(const output of index[transaction.transactionName] ?? [])
+                for(const childTransaction of output)
+                    collect(childTransaction[0]);
+        }
+        roots.forEach(collect);
+        return Object.entries(collected).map(
+            ([name, list]) => verticalAlignmentLine(list, VERTICAL_ALIGNMENT_WEIGHTS[name]));
+    }
+
+    function alignmentLines(): string[] {
+        const roots = transactions.filter(transaction => transaction.inputs.length === 0);
+        return [
+            horizontalAlignmentLine(roots, 'min'),
+            ...verticalAlignmentLines(roots)
         ];
     }
 
-    return `digraph BitSnark {${transactions.reduce((dot, transaction) => dot +
-        `\n\t${transactionLines(transaction).join('\n\t')}`, '')}\n}`;
+    return `digraph BitSnark {${['',
+        ...transactions.map(transactionLine),
+        ...alignmentLines(),
+        ...transactions.flatMap(outputLines)
+    ].join('\n\t')}\n}`;
 }
 
 const scriptName = __filename;
