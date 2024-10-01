4c01428a374a96ce99e0840fe7b68c606b2f1964
Derek Matter <dharkmattr@gmail.com>
2024-10-01 16:56:43 +0300

    fixed schema

--- src/agent/db-schema.sql
+++ src/agent/db-schema.sql
@@ -1,29 +1,12 @@
--- Database: bitsnark
 
-CREATE DATABASE postgres
-    WITH 
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.UTF-8'
-    LC_CTYPE = 'en_US.UTF-8'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-
--- Table: public.transaction_templates
 
 CREATE TABLE IF NOT EXISTS public.transaction_templates
 (
-    "agentId" character varying COLLATE pg_catalog."default" NOT NULL,
-    "setupId" character varying COLLATE pg_catalog."default" NOT NULL,
-    name character varying COLLATE pg_catalog."default" NOT NULL,
+    "agentId" character varying NOT NULL,
+    "setupId" character varying NOT NULL,
+    name character varying NOT NULL,
     object json NOT NULL,
-    "txId" character varying COLLATE pg_catalog."default",
+    "txId" character varying,
     ordinal integer,
     CONSTRAINT transaction_template_pkey PRIMARY KEY ("agentId", "setupId", name)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.transaction_templates
-    OWNER to postgres;
-
+);
