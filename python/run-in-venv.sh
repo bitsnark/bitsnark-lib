b47afb495d3d4a57ddaff2f845a9941f1c6dbcfd
root <root@chakra>
2024-10-10 15:25:19 +0300

    Allow overriding initial python implementation

--- python/run-in-venv.sh
+++ python/run-in-venv.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 venv_name=venv
+python_command="${PYTHON:-python3}"
 script_dir="$(dirname "$(realpath "$0")")"
 repo_root_dir="$(git -C "$script_dir" rev-parse --show-toplevel)"
 venv_dir="$script_dir/$venv_name"
 if ! [ -d "$venv_dir" ]; then
     echo "Creating virtual environment in $venv_dir"
-    python3 -m venv "$venv_dir"
+    "$python_command" -m venv "$venv_dir"
     . "$venv_dir/bin/activate"
     pip install -r "$script_dir/requirements.txt"
 fi
