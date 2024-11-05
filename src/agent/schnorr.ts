ca1c195b77d5be97468ac5ac0160436251fb8b1f
gula <gmemail@gmail.com>
2024-11-05 08:50:20 +0200

    clean debug

--- src/agent/schnorr.ts
+++ src/agent/schnorr.ts
@@ -43,48 +43,10 @@ if (__filename === process.argv[1]) {
     console.log('Private Key:', keypair.privateKey, keypair.privateKey.length);
     console.log('Public Key:', keypair.publicKey, keypair.publicKey.length);
 
-    const myMessage = `Hello, World!
-        This is a message from the Bitsnark team.
-        We are testing Schnorr signatures!
-        Have a great day!`;
-
-
-    // Sign the message
-    const me = { private: keypair.privateKey, public: keypair.publicKey };
-
-    const isPoint = secp256k1.isPoint(Buffer.from(me.public, 'hex'));
-
-    console.log('Is point:', isPoint);
-    console.log('length', Buffer.from(me.public, 'hex').length);
-    const signature = signMessage(myMessage, me.private);
+    const myMessage = `Hello, World!`;
+    const signature = signMessage(`Hello, World!`, keypair.privateKey);
     console.log('Signature:', signature);
 
-    // Verify the message
-    const verified = verifyMessage(myMessage, signature, me.public);
-    console.log('Message verified by me:', verified);
-
-    //the keys we have in the config file
-    const keyPairs = {
-        bitsnark_prover_1: {
-            public: process.env['PROVER_SCHNORR_PUBLIC'] ?? '02ae2ea39bca4b6b14567e3c38b9680f6483ceeef4ae17f8dceb5a5a0866999b75',
-            private: process.env['PROVER_SCHNORR_PRIVATE'] ?? '415c69b837f4146019574f59c223054c8c144ac61b6ae87bc26824c0f8d034e2'
-        },
-        bitsnark_verifier_1: {
-            public: process.env['VERIFIER_SCHNORR_PUBLIC'] ?? '0386ad52a51b65ab3aed9a64e7202a7aa1f2bd3da7a6a2dae0f5c8e28bda29de79',
-            private: process.env['VERIFIER_SCHNORR_PRIVATE'] ?? 'd4067af1132afcb352b0edef53d8aa2a5fc713df61dee31b1d937e69ece0ebf0'
-        }
-    }
-
-    const pkb = secp256k1.xOnlyPointFromScalar(Buffer.from(keyPairs.bitsnark_prover_1.private, 'hex'));
-    const vkb = secp256k1.xOnlyPointFromScalar(Buffer.from(keyPairs.bitsnark_verifier_1.private, 'hex'));
-    console.log('prover public key is:', keyPairs.bitsnark_prover_1.public, 'and should be:', Buffer.from(pkb).toString('hex'));
-    console.log('verifier public key:', keyPairs.bitsnark_verifier_1.public, 'and should be:', Buffer.from(vkb).toString('hex'));
-
-    //using the current keys
-    try {
-        const confverified = verifyMessage(myMessage, signature, keyPairs.bitsnark_prover_1.public);
-        console.log('Message verified by config:', confverified);
-    } catch (e) {
-        console.log('verify Schnorr error: old public is not fit for Schnorr for it is not the KUITZAT HADARC but of 33 length. \n', e);
-    }
+    const verified = verifyMessage(myMessage, signature, keypair.publicKey);
+    console.log('Is message verified?', verified);
 }
