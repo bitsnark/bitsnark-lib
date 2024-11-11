e6fe59e2832b004fe71e3531a0855b54a7ca3158
root <root@chakra>
2024-11-11 20:37:38 +0200

    Setup prettier integration

---
+++ scripts/prettier.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+prettier_flag="$([ "$1" = "--fix" ] && echo "--write" || echo "--check")"
+script_dir="$(dirname "$(realpath "$0")")"
+repo_root_dir="$(git -C "$script_dir" rev-parse --show-toplevel)"
+dirs="src tests analysis"
+(for dir in $dirs; do
+    find "$repo_root_dir/$dir" -type f -name '*.ts'
+done) | xargs -P$(nproc) npx prettier $prettier_flag
