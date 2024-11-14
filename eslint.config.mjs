a3609abe5810e74491b1c4a27b7656c438ddcf4b
gula <gmemail@gmail.com>
2024-11-14 12:37:11 +0200

    PR fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,7 @@ export default tseslint.config(
         rules: {
             'unused-imports-ts/no-unused-imports': 'error',
             '@typescript-eslint/no-explicit-any': 'off',
-            '@typescript-eslint/no-require-imports': 'error',
+            '@typescript-eslint/no-require-imports': 'off',
             '@typescript-eslint/no-unused-vars': 'off'
         },
         languageOptions: {
