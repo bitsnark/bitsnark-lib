b9b254520e19fe2c412df1dfe8321670b05de34c
Derek Matter <dharkmattr@gmail.com>
2024-11-15 13:10:40 +0200

    removed linter rule for while(true)'

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,8 @@ export default tseslint.config(
             'unused-imports-ts/no-unused-imports': 'error',
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-require-imports': 'off',
-            '@typescript-eslint/no-unused-vars': 'off'
+            '@typescript-eslint/no-unused-vars': 'off',
+            '@typescript-eslint/no-constant-condition': 'off'
         },
         languageOptions: {
             globals: {
