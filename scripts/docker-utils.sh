c8a4f6240c43f1e56c854d8dbe5e6ae94cb9f176
root <root@chakra>
2024-11-16 13:15:59 +0200

    Script fixes

--- scripts/docker-utils.sh
+++ scripts/docker-utils.sh
@@ -1,18 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
-# Function to check if a Docker container exists and handle it
-prompt_delete_container() {
-    local CONTAINER_NAME=$1
-
-    if [ ! "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
-        return 0;
-    fi
-
+# Function to check if a Docker container exists and handle it.
+conditionally_remove_container() {
+    local CONTAINER_NAME="$1"
+    test -z "$(docker ps -aq -f name=$CONTAINER_NAME)" && return 0
     echo "Container $CONTAINER_NAME already exists."
     read -p "Do you want to remove the existing container? (y/n): " choice
-        case "$choice" in
-            y|Y ) docker rm -f $CONTAINER_NAME; return 0;;
-            * ) echo "FAIL. Old container was not removed."; return 1;;
-        esac
-
+    if [ "$choice" = y ] || [ "$choice" = Y ]; then
+        docker rm -f "$CONTAINER_NAME" && return 0
+    fi
+    echo Existing container was not removed - exiting.
+    return 1
 }
--- scripts/start-db.sh
+++ scripts/start-db.sh
@@ -2,10 +2,11 @@
 
 . ./scripts/docker-utils.sh
 
-CONTAINER_NAME="postgres"
+CONTAINER_NAME=postgres
 
-# returns 1 if container exists but not removed
-prompt_delete_container $CONTAINER_NAME
+# Will exit if the container remains running.
+conditionally_remove_container "$CONTAINER_NAME"
 
-# Run the PostgreSQL container and apply the schema
-docker run --name $CONTAINER_NAME -e POSTGRES_PASSWORD=1234 -d -p 5432:5432 -v  ./db/schema.sql:/docker-entrypoint-initdb.d/schema.sql postgres
+# Run the PostgreSQL container and apply the schema.
+docker run --name "$CONTAINER_NAME" -e POSTGRES_PASSWORD=1234 -d -p 5432:5432 \
+    -v ./db/schema.sql:/docker-entrypoint-initdb.d/schema.sql postgres
