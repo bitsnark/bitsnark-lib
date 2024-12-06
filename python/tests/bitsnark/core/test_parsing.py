70db8f5430f8fe4b75e7caaeb8816df2907fe052
Derek Matter <dharkmattr@gmail.com>
2024-12-06 10:09:12 +0200

    fixed python tests

--- python/tests/bitsnark/core/test_parsing.py
+++ python/tests/bitsnark/core/test_parsing.py
@@ -2,16 +2,16 @@ from bitsnark.core.parsing import parse_bignum, parse_hex_str, parse_hex_bytes
 
 
 def test_parse_bignum():
-    assert parse_bignum("0x3b9aca00n") == 0x3b9aca00
+    assert parse_bignum("bigint:3b9aca00n") == 0x3b9aca00
     pass
 
 
 def test_parse_hex_str():
-    assert parse_hex_str("hex:51201c5b544e53ee4c965d3c91f8df230af3d41adb51067b4f2f38074c6945797b6d") == \
+    assert parse_hex_str("Buffer:51201c5b544e53ee4c965d3c91f8df230af3d41adb51067b4f2f38074c6945797b6d") == \
         "51201c5b544e53ee4c965d3c91f8df230af3d41adb51067b4f2f38074c6945797b6d"
 
 
 def tet_parse_hex_bytes():
-    assert parse_hex_bytes("hex:51201c5b544e53ee4c965d3c91f8df230af3d41adb51067b4f2f38074c6945797b6d") == \
+    assert parse_hex_bytes("Buffer:51201c5b544e53ee4c965d3c91f8df230af3d41adb51067b4f2f38074c6945797b6d") == \
            bytes.fromhex("51201c5b544e53ee4c965d3c91f8df230af3d41adb51067b4f2f38074c6945797b6d")
 
