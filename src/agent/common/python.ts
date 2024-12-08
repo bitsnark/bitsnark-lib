388ac10f7628840e109048cdeaa2500cda854b6d
root <root@chakra>
2024-12-08 14:23:43 +0200

    Better Python error

--- src/agent/common/python.ts
+++ src/agent/common/python.ts
@@ -14,11 +14,10 @@ export async function runPython(args: string[]): Promise<Buffer> {
         return result;
     } catch (error: unknown) {
         const subprocessError = error as { status: number; stdout: Buffer; stderr: Buffer };
-        console.error(
-            `Python script failed with code ${subprocessError.status}\n` +
-                `stdout:\n${subprocessError.stdout.toString()}\n` +
-                `stderr:\n${subprocessError.stderr.toString()}\n`
-        );
-        throw error;
+        const errorMessage = `Python script failed with code ${subprocessError.status}\n` +
+            `stdout:\n${subprocessError.stdout.toString()}\n` +
+            `stderr:\n${subprocessError.stderr.toString()}\n`
+        console.error(errorMessage);
+        throw new Error(errorMessage);
     }
 }
