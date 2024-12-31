30ec6646f99e4ae39452e21cd654d0f96a2356ac
Israel <40337+israellevin@users.noreply.github.com>
2024-12-31 21:55:35 +0200

    Set DYLD lib env var before running Python
    You never know at which point MacOS is going to delete it.

--- scripts/python-test.sh
+++ scripts/python-test.sh
@@ -2,6 +2,8 @@
 
 . "$(dirname "$(realpath "$0")")/common.sh"
 activate_python_venv
+DYLD_FALLBACK_LIBRARY_PATH="$DYLD_FALLBACK_LIBRARY_PATH:/usr/local/lib:/opt/homebrew/lib"
+export DYLD_FALLBACK_LIBRARY_PATH
 
 cd python
 BITCOIN_NODE_PORT=19443
