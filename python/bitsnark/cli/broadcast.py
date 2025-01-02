ba7dd26c28a1bedd0497f3415e7ed1df5da77972
Rainer Koirikivi <rainer@koirikivi.fi>
2025-01-02 18:10:32 +0200

    Add tx size data to show command (and refactor broadcast slightly)

--- python/bitsnark/cli/broadcast.py
+++ python/bitsnark/cli/broadcast.py
@@ -3,6 +3,7 @@ import argparse
 import itertools
 import logging
 from sqlalchemy import select
+from sqlalchemy.orm.session import Session
 
 from bitcointx.core import CMutableTransaction, CTxInWitness, CTxWitness
 from bitcointx.core.script import CScript, CScriptWitness
@@ -20,16 +21,67 @@ def broadcast_transaction(
     dbsession,
     bitcoin_rpc,
     evaluate_inputs: bool = False,
-    no_test_mempool_accept: bool = False
+    no_test_mempool_accept: bool = False,
+    dump: bool = False,
 ) -> str:
     logger.info("Attempting to broadcast %s", tx_template.name)
+    tx = create_tx_with_witness(
+        tx_template=tx_template,
+        dbsession=dbsession,
+    )
+
+    if evaluate_inputs:
+        for input_index, input_witness in enumerate(tx.wit.vtxinwit):
+            logger.info("Evaluating input %s", input_index)
+            *witness_elems, tapscript, _ = input_witness.scriptWitness.stack
+            eval_tapscript(
+                witness_elems=witness_elems,
+                script=CScript(tapscript),
+                inIdx=input_index,
+                txTo=tx,
+                # TODO: would be swell to get it working without ignore_signature_errors!
+                ignore_signature_errors=True,
+            )
+
+    signed_serialized_tx = tx.serialize().hex()
+
+    if dump:
+        dump_filename = f"{tx_template.name}-signed-serialized-.dump"
+        with open(dump_filename, "w") as f:
+            f.write(signed_serialized_tx)
+        print("Dump written to", dump_filename)
+
+    if not no_test_mempool_accept:
+        mempoolaccept_ret = bitcoin_rpc.call(
+            'testmempoolaccept',
+            [signed_serialized_tx],
+        )
+        logger.info("Test mempool accept result: %s", mempoolaccept_ret)
+        if not mempoolaccept_ret[0]['allowed']:
+            raise ValueError(
+                f"Transaction {tx_template.name!r} not accepted by mempool: {mempoolaccept_ret[0]['reject-reason']}")
+
+    txid = bitcoin_rpc.call('sendrawtransaction', signed_serialized_tx)
+    assert txid == tx_template.txid
+    logger.info("Transaction broadcast: %s", txid)
+    return txid
+
+
+def create_tx_with_witness(
+    *,
+    tx_template: TransactionTemplate,
+    dbsession: Session,
+) -> CMutableTransaction:
     signed_serialized_tx = tx_template.tx_data.get('signedSerializedTx')
     if not signed_serialized_tx:
         raise ValueError(f"Transaction {tx_template.name} has no signedSerializedTx")
 
     signed_serialized_tx = parse_hex_str(signed_serialized_tx)
 
+    # TODO: signedSerializedTx is a total misnomer here as it doesn't contain any signature data
     tx = CMutableTransaction.deserialize(bytes.fromhex(signed_serialized_tx))
+    if tx.wit is not None and tx.wit.serialize().strip(b'\x00'):
+        raise ValueError(f"Transaction {tx_template.name} already has witness data")
     input_witnesses = []
 
     for input_index, inp in enumerate(tx_template.inputs):
@@ -79,36 +131,7 @@ def broadcast_transaction(
         input_witnesses.append(input_witness)
 
     tx.wit = CTxWitness(vtxinwit=input_witnesses)
-
-    if evaluate_inputs:
-        for input_index, input_witness in enumerate(input_witnesses):
-            logger.info("Evaluating input %s", input_index)
-            *witness_elems, tapscript, _ = input_witness.scriptWitness.stack
-            eval_tapscript(
-                witness_elems=witness_elems,
-                script=CScript(tapscript),
-                inIdx=input_index,
-                txTo=tx,
-                # TODO: would be swell to get it working without ignore_signature_errors!
-                ignore_signature_errors=True,
-            )
-
-    signed_serialized_tx = tx.serialize().hex()
-
-    if not no_test_mempool_accept:
-        mempoolaccept_ret = bitcoin_rpc.call(
-            'testmempoolaccept',
-            [signed_serialized_tx],
-        )
-        logger.info("Test mempool accept result: %s", mempoolaccept_ret)
-        if not mempoolaccept_ret[0]['allowed']:
-            raise ValueError(
-                f"Transaction {tx_template.name!r} not accepted by mempool: {mempoolaccept_ret[0]['reject-reason']}")
-
-    txid = bitcoin_rpc.call('sendrawtransaction', signed_serialized_tx)
-    assert txid == tx_template.txid
-    logger.info("Transaction broadcast: %s", txid)
-    return txid
+    return tx
 
 
 class BroadcastCommand(Command):
@@ -129,6 +152,11 @@ class BroadcastCommand(Command):
             help='Evaluate input tapscript execution (experimental)',
             action='store_true',
         )
+        parser.add_argument(
+            '--dump',
+            help='Dump tx as hex',
+            action='store_true',
+        )
 
     def run(
         self,
@@ -139,10 +167,12 @@ class BroadcastCommand(Command):
         dbsession = context.dbsession
         evaluate_inputs = getattr(context.args, 'eval_inputs')
         no_test_mempool_accept = getattr(context.args, 'no_test_mempool_accept')
+        dump = getattr(context.args, 'dump', None)
         return broadcast_transaction(
             tx_template=tx_template,
             dbsession=dbsession,
             bitcoin_rpc=bitcoin_rpc,
             evaluate_inputs=evaluate_inputs,
             no_test_mempool_accept=no_test_mempool_accept,
+            dump=dump,
         )
--- python/bitsnark/cli/show.py
+++ python/bitsnark/cli/show.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import io
 
 from bitcointx.core.script import CScript
 from bitcointx.wallet import CCoinAddress
@@ -9,6 +10,7 @@ import sqlalchemy as sa
 from bitsnark.core.models import TransactionTemplate
 from bitsnark.core.parsing import parse_bignum, parse_hex_bytes
 from ._base import Command, add_tx_template_args, find_tx_template, Context
+from .broadcast import create_tx_with_witness
 
 
 class ShowCommand(Command):
@@ -76,3 +78,12 @@ class ShowCommand(Command):
                         value = value[:maxwidth] + "..."
                     key = f"{key}:".ljust(25)
                     print(f'      - {key} {value}')
+
+        # Deserialized tx stuff
+        tx = create_tx_with_witness(tx_template=tx_template, dbsession=dbsession)
+        print(f"Transaction virtual size: {tx.get_virtual_size()} vB")
+        witness_size = len(tx.wit.serialize())
+        print(
+            f"Witness size: {witness_size} B ({witness_size / 4} vB, "
+            f"{witness_size / 4 / tx.get_virtual_size() * 100:.2f}% of tx size)"
+        )
