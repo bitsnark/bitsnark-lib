70f71e3ab9e451cfe864015883a1ab7c4beef9b3
Rainer Koirikivi <rainer@koirikivi.fi>
2025-01-10 12:38:09 +0200

    Make sure node commands use the right bitcoin rpc when run by pytest

--- python/tests/constants.py
+++ python/tests/constants.py
@@ -14,6 +14,30 @@ POSTGRES_URL_PROVER = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGR
 POSTGRES_DATABASE_VERIFIER = 'bitsnark_verifier_1'
 POSTGRES_URL_VERIFIER = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE_VERIFIER}'
 
-BITCOIN_RPC_URL = 'http://rpcuser:rpcpassword@localhost:19443'
-
 DB_SCHEMA_FILE = PROJECT_ROOT_DIR / 'db' / 'schema.sql'
+
+BITCOIN_RPC_USER = 'rpcuser'
+BITCOIN_RPC_PASSWORD = 'rpcpassword'
+BITCOIN_RPC_HOST = 'localhost'
+BITCOIN_RPC_PORT = 19443
+BITCOIN_RPC_URL = f'http://{BITCOIN_RPC_USER}:{BITCOIN_RPC_PASSWORD}@{BITCOIN_RPC_HOST}:{BITCOIN_RPC_PORT}'
+
+# nodejs (e.g. npm run emulate-setup) reads .env and then fall's back to defaults in agent.conf.ts,
+# but environment variables supplied by us have precedence
+# we want to both supply our own values for database and bitcoin node, and also make sure that
+# we don't accidentally use the values from the .env file.
+NODE_ENV = {
+    'POSTGRES_USER': POSTGRES_USER,
+    'POSTGRES_PASSWORD': POSTGRES_PASSWORD,
+    'POSTGRES_HOST': POSTGRES_HOST,
+    'POSTGRES_PORT': str(POSTGRES_PORT),
+    'BITCOIN_NODE_NETWORK': 'regtest',
+    'BITCOIN_NODE_USERNAME': BITCOIN_RPC_USER,
+    'BITCOIN_NODE_PASSWORD': BITCOIN_RPC_PASSWORD,
+    'BITCOIN_NODE_HOST': BITCOIN_RPC_HOST,
+    'BITCOIN_NODE_PORT': str(BITCOIN_RPC_PORT),
+    'PROVER_SCHNORR_PRIVATE': '415c69b837f4146019574f59c223054c8c144ac61b6ae87bc26824c0f8d034e2',
+    'PROVER_SCHNORR_PUBLIC': 'ae2ea39bca4b6b14567e3c38b9680f6483ceeef4ae17f8dceb5a5a0866999b75',
+    'VERIFIER_SCHNORR_PRIVATE': 'd4067af1132afcb352b0edef53d8aa2a5fc713df61dee31b1d937e69ece0ebf0',
+    'VERIFIER_SCHNORR_PUBLIC': '86ad52a51b65ab3aed9a64e7202a7aa1f2bd3da7a6a2dae0f5c8e28bda29de79',
+}
--- python/tests/utils/npm.py
+++ python/tests/utils/npm.py
@@ -2,12 +2,7 @@ import logging
 import os
 import subprocess
 from bitsnark.constants import PROJECT_ROOT_DIR
-from ..constants import (
-    POSTGRES_USER,
-    POSTGRES_PASSWORD,
-    POSTGRES_HOST,
-    POSTGRES_PORT,
-)
+from ..constants import NODE_ENV
 
 
 logger = logging.getLogger(__name__)
@@ -16,17 +11,6 @@ logger = logging.getLogger(__name__)
 class NPMCommandRunner:
     def __init__(self):
         self.cwd = PROJECT_ROOT_DIR
-        self.env = {
-            'POSTGRES_USER': POSTGRES_USER,
-            'POSTGRES_PASSWORD': POSTGRES_PASSWORD,
-            'POSTGRES_HOST': POSTGRES_HOST,
-            'POSTGRES_PORT': str(POSTGRES_PORT),
-            # We need to make sure ts-node doesn't read these from .env even if it exists
-            'PROVER_SCHNORR_PRIVATE': '415c69b837f4146019574f59c223054c8c144ac61b6ae87bc26824c0f8d034e2',
-            'PROVER_SCHNORR_PUBLIC': 'ae2ea39bca4b6b14567e3c38b9680f6483ceeef4ae17f8dceb5a5a0866999b75',
-            'VERIFIER_SCHNORR_PRIVATE': 'd4067af1132afcb352b0edef53d8aa2a5fc713df61dee31b1d937e69ece0ebf0',
-            'VERIFIER_SCHNORR_PUBLIC': '86ad52a51b65ab3aed9a64e7202a7aa1f2bd3da7a6a2dae0f5c8e28bda29de79',
-        }
 
     def run(self, command: str, *args: str) -> str:
         try:
@@ -35,7 +19,7 @@ class NPMCommandRunner:
                 cwd=self.cwd,
                 env={
                     **os.environ,
-                    **self.env,
+                    **NODE_ENV,
                 },
                 text=True,
             )
