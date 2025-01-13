620758353f57cceedb9ac1958234b59d7ace9b7e
Rainer Koirikivi <rainer@koirikivi.fi>
2025-01-13 21:09:09 +0200

    Add more taproot test cases

--- tests/taproot/compressor.spec.ts
+++ tests/taproot/compressor.spec.ts
@@ -68,7 +68,29 @@ describe('SimpleTapTree', () => {
                     'c0e0dfe2300b0dd746a3f8674dfd4525623639042569d829c7f0eed9602d263e6fba982a91d4fc552163cb1c0da03676102d5b7a014304c01f0c77b2b8e888de1ce6bdf7b991061435ac56d465d7364da29bbb7ee3a1aa751576fde591ea4869b5'
                 ]
             }
-        }
+        },
+        {
+            "given": {
+                "scripts": [
+                    "0087",
+                    "5187",
+                    "5287",
+                    "5387",
+                    "5487"
+                ]
+            },
+            "expected": {
+                "root": "25f3c168e623516a205bfda9792707b1ba678e29c670bc99a5b237efb98bae88",
+                "scriptPubKey": "51206b0e0c47122063d5707d73925b8ebf6994d076ff35c557f27f45f879256d90cb",
+                "scriptPathControlBlocks": [
+                    "c1e0dfe2300b0dd746a3f8674dfd4525623639042569d829c7f0eed9602d263e6f6b13becdaf0eee497e2f304adcfa1c0c9e84561c9989b7f2b5fc39f5f90a60f6f544fe4afa992838d797c3b6d771c688bb73cd679e0e85b822ab71ac6c784c9f6c55251c400ed27c375fbe1c171508c4eec473ecd10cf88532b735cbe7e6f64c",
+                    "c1e0dfe2300b0dd746a3f8674dfd4525623639042569d829c7f0eed9602d263e6f8988aa92472aa752c39281a192217284d6b7c6cb6226917bcd14a187cd7d2ac3f544fe4afa992838d797c3b6d771c688bb73cd679e0e85b822ab71ac6c784c9f6c55251c400ed27c375fbe1c171508c4eec473ecd10cf88532b735cbe7e6f64c",
+                    "c1e0dfe2300b0dd746a3f8674dfd4525623639042569d829c7f0eed9602d263e6f160bd30406f8d5333be044e6d2d14624470495da8a3f91242ce338599b2339312b702af5f508077b490b349c28e89c3fa9f50bc4e89a9845fb930b4a78af18966c55251c400ed27c375fbe1c171508c4eec473ecd10cf88532b735cbe7e6f64c",
+                    "c1e0dfe2300b0dd746a3f8674dfd4525623639042569d829c7f0eed9602d263e6fed5af8352e2a54cce8d3ea326beb7907efa850bdfe3711cef9060c7bb5bcf59e2b702af5f508077b490b349c28e89c3fa9f50bc4e89a9845fb930b4a78af18966c55251c400ed27c375fbe1c171508c4eec473ecd10cf88532b735cbe7e6f64c",
+                    "c1e0dfe2300b0dd746a3f8674dfd4525623639042569d829c7f0eed9602d263e6fbf2c4bf1ca72f7b8538e9df9bdfd3ba4c305ad11587f12bbfafa00d58ad6051dbb3dcc2dee1c80f8c8b08eed51395b36ecb4305cdf88200b2acdec14b0c710aef57e223c32dd103a1302dcf54dc5394bf2a812596813d090883305451427d084"
+                ]
+            }
+        },
     ];
 
     it.each(testCases)('scriptPubKey test case %#', (testcase: Testcase) => {
